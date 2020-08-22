dfcb6ccebb747f970005fb4d5ba2a258bad0f6bf
Alex Skudrovskis <alex.skudrovskis@gmail.com>
2020-08-22 13:37:15 -0600

    const variable spelling fix

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,7 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
-const generateMakrdown = require ("./Develop/generateMarkdown");
+const generateMarkdown = require ("./Develop/generateMarkdown");
 
 // array of questions for user
 const questions = () => {
