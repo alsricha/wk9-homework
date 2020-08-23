c99c14b92e4b4bd2985b22ef0e0004f448e8a281
Alex Skudrovskis <alex.skudrovskis@gmail.com>
2020-08-23 14:18:56 -0600

    Update Repo

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,7 @@
-const inquirer = require("inquirer");
 const fs = require("fs");
+const inquirer = require("inquirer");
 const util = require("util");
-const generateMarkdown = require ("./Develop/generateMarkdown");
+const generateMarkdown = require ("./Develop/utils/generateMarkdown");
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
